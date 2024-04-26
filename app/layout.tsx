6f43bdc16b429de2fad777253b809da8b8a32671
codyzzd <codyzzd@gmail.com>
2024-04-26 19:47:10 -0300

    last fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,24 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const AeonikTRIAL = localFont({
+  src: [
+    {
+      path: "../public/fonts/AeonikTRIAL-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+
+    {
+      path: '../public/fonts/AeonikTRIAL-Bold.otf',
+      weight: '700',
+      style: 'bold',
+    },
+  ],
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/sliders/areas.tsx
+++ app/sliders/areas.tsx
@@ -32,91 +32,89 @@ export function EmblaCarousel() {
     <div className="embla">
       <div className="flex flex-row justify-center mb-4 gap-2">
         <button
-          className="embla__prev hover:bg-slate-100 w-10 h-10 border-[1px] border-slate-200"
+          className="embla__prev  w-10 h-10 bg-[#D6B469] rounded-lg hover:bg-yellow-200 "
           onClick={scrollPrev}>
           <FontAwesomeIcon icon={faChevronLeft} />
         </button>
         <button
-          className="embla__next hover:bg-slate-100 w-10 h-10 border-[1px] border-slate-200"
+          className="embla__next  w-10 h-10 bg-[#D6B469] rounded-lg hover:bg-yellow-200"
           onClick={scrollNext}>
           <FontAwesomeIcon icon={faChevronRight} />
         </button>
       </div>
-      <div className="embla__dots">
-         
-        </div>
+      <div className="embla__dots"></div>
       <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container [&_h3]:font-bold [&_h3]:text-lg ">
+        <div className="embla__container [&_h3]:font-bold [&_h3]:text-sm ">
           <div className="embla__slide flex flex-col justify-center">
             <div
               className="rounded-[48px] h-[400px]"
               style={{
-                backgroundImage: 'url("/images/area_mate.avif")',
+                backgroundImage: 'url("/images/areas/area_mate.avif")',
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}></div>
-            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center">
-              Salário Maternidade
+            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center uppercase">
+              Salário-Maternidade
             </h3>
           </div>
           <div className="embla__slide flex flex-col ">
             <div
               className="h-[400px] rounded-[48px]"
               style={{
-                backgroundImage: 'url("/images/area_mate.avif")',
+                backgroundImage: 'url("/images/areas/area_apos.avif")',
                 backgroundSize: "cover",
-                backgroundPosition: "center",
+                backgroundPosition: "center left",
               }}></div>
-            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center">
-              Salário Maternidade
+            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center uppercase">
+              Aposentadoria por idade
             </h3>
           </div>
           <div className="embla__slide flex flex-col ">
             <div
               className="h-[400px] rounded-[48px]"
               style={{
-                backgroundImage: 'url("/images/area_mate.avif")',
+                backgroundImage: 'url("/images/areas/area_inva.avif")',
                 backgroundSize: "cover",
-                backgroundPosition: "center",
+                backgroundPosition: "center right",
               }}></div>
-            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center">
-              Salário Maternidade
+            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center uppercase">
+              Aposentadoria por invalidez
             </h3>
           </div>
           <div className="embla__slide flex flex-col ">
             <div
               className="h-[400px] rounded-[48px]"
               style={{
-                backgroundImage: 'url("/images/area_mate.avif")',
+                backgroundImage: 'url("/images/areas/area_doen.avif")',
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}></div>
-            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center">
-              Salário Maternidade
+            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center uppercase">
+              Auxílio-doença
             </h3>
           </div>
           <div className="embla__slide flex flex-col ">
             <div
               className="h-[400px] rounded-[48px]"
               style={{
-                backgroundImage: 'url("/images/area_mate.avif")',
+                backgroundImage: 'url("/images/areas/area_loas.avif")',
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}></div>
-            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center">
-              Salário Maternidade
+            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center uppercase">
+              Loas para deficientes
             </h3>
           </div>
           <div className="embla__slide flex flex-col ">
             <div
               className="h-[400px] rounded-[48px]"
               style={{
-                backgroundImage: 'url("/images/area_mate.avif")',
+                backgroundImage: 'url("/images/areas/area_old.avif")',
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}></div>
-            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center">
-              Salário Maternidade
+            <h3 className="p-3 bg-[#F5DDA7] -translate-y-1/2 w-fit rounded-2xl shadow-md self-center uppercase">
+              Loas para idosos
             </h3>
           </div>
         </div>
